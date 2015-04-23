14853f7fc7d6fbfac6a8302425fc2d7f33ae65b0
wurh <wurh@wurhdeMacBook-Air.local>
2015-04-23 10:24:08 +0800

    [INIT] rush update

--- src/index.js
+++ src/index.js
@@ -7,80 +7,128 @@ var table;
 
 function doInject() {
     var isDiv = document.getElementById('injected');
-    if(isDiv){
+    if (isDiv) {
         return;
     }
     var div = document.createElement('div');
-    div.setAttribute('id','injected');
+    div.setAttribute('id', 'injected');
     document.body.appendChild(div);
     startGetPc();
-    setInterval(function(){
+    setInterval(function () {
         startGetPc();
-    },7000);
+    }, 4500);
 }
 
 
-
-function startGetPc(){
+function startGetPc() {
     getMacMini2();
-    doGet(getLianxiang,1000);
-    doGet(getLianxiang2,2000);
-    doGet(getMacMini1,3000);
-    doGet(getMacMini3,4000);
-    doGet(getMacMini4,5000);
-    doGet(getMacMini5,6000);
-    doGet(getMacMini6,7000);
+    doGet(getLianxiang, 500);
+    doGet(getLianxiang2, 1500);
+    doGet(getMacMini1, 2000);
+    doGet(getLianxiang3, 2500);
+    doGet(getMacMini3, 3000);
+    doGet(getMacMini4, 3500);
+    doGet(getMacMini5, 4000);
+    doGet(getMacMini6, 4500);
 }
-function doGet(func,times){
-    setTimeout(function(){
+function doGet(func, times) {
+    setTimeout(function () {
         func();
-    },times);
+    }, times);
 }
 
-function getMacMini1(){
-    var input  = table.querySelectorAll('tbody tr')[1].querySelectorAll('td')[7].querySelectorAll('input')[0];
-    console.log('m1')
-    input.click();
+function getMacMini1() {
+    try {
+        var input = table.querySelectorAll('tbody tr')[1].querySelectorAll('td')[7].querySelectorAll('input')[0];
+        console.log('m1 waiting')
+        input.click();
+    } catch (e) {
+        console.log('m1 error')
+    }
 }
 
-function getMacMini2(){
-    var input  = table.querySelectorAll('tbody tr')[9].querySelectorAll('td')[7].querySelectorAll('input')[0];
-    console.log('m2')
-    input.click();
+function getMacMini2() {
+    try {
+        var input = table.querySelectorAll('tbody tr')[8].querySelectorAll('td')[7].querySelectorAll('input')[0];
+        console.log('m2 waiting')
+        input.click();
+    } catch (e) {
+        console.log('m2 error')
+    }
+
 }
 
-function getMacMini3(){
-    var input  = table.querySelectorAll('tbody tr')[2].querySelectorAll('td')[7].querySelectorAll('input')[0];
-    console.log('m3')
-    input.click();
+function getMacMini3() {
+    try {
+        var input = table.querySelectorAll('tbody tr')[2].querySelectorAll('td')[7].querySelectorAll('input')[0];
+        console.log('m3 waiting')
+        input.click();
+    } catch (e) {
+        console.log('m3 error')
+    }
+
 }
 
-function getMacMini4(){
-    var input  = table.querySelectorAll('tbody tr')[3].querySelectorAll('td')[7].querySelectorAll('input')[0];
-    console.log('m4')
-    input.click();
+function getMacMini4() {
+    try {
+        var input = table.querySelectorAll('tbody tr')[3].querySelectorAll('td')[7].querySelectorAll('input')[0];
+        console.log('m4 waiting')
+        input.click();
+    } catch (e) {
+        console.log('m4 error')
+    }
 }
 
-function getMacMini5(){
-    var input  = table.querySelectorAll('tbody tr')[4].querySelectorAll('td')[7].querySelectorAll('input')[0];
-    console.log('m5')
-    input.click();
+function getMacMini5() {
+    try {
+        var input = table.querySelectorAll('tbody tr')[4].querySelectorAll('td')[7].querySelectorAll('input')[0];
+        console.log('m5 waiting')
+        input.click();
+    } catch (e) {
+        console.log('m5 error')
+    }
+
 }
 
-function getMacMini6(){
-    var input  = table.querySelectorAll('tbody tr')[6].querySelectorAll('td')[7].querySelectorAll('input')[0];
-    console.log('m6')
-    input.click();
+function getMacMini6() {
+    try {
+        var input = table.querySelectorAll('tbody tr')[6].querySelectorAll('td')[7].querySelectorAll('input')[0];
+        console.log('m6 waiting')
+        input.click();
+    } catch (e) {
+        console.log('m6 error')
+    }
+
 }
 
-function getLianxiang(){
-    var input  = table.querySelectorAll('tbody tr')[34].querySelectorAll('td')[7].querySelectorAll('input')[0];
-    console.log('liangxiang')
-    input.click();
+function getLianxiang() {
+    try {
+        var input = table.querySelectorAll('tbody tr')[34].querySelectorAll('td')[7].querySelectorAll('input')[0];
+        console.log('liangxiang waiting')
+        input.click();
+    } catch (e) {
+        console.log('liangxiang error')
+    }
+
 }
 
-function getLianxiang2(){
-    var input  = table.querySelectorAll('tbody tr')[75].querySelectorAll('td')[7].querySelectorAll('input')[0];
-    console.log('Lianxiang2')
-    input.click();
+function getLianxiang2() {
+    try {
+        var input = table.querySelectorAll('tbody tr')[74].querySelectorAll('td')[7].querySelectorAll('input')[0];
+        console.log('Lianxiang2 waiting')
+        input.click();
+    } catch (e) {
+        console.log('Lianxiang2 error')
+    }
+
+}
+
+function getLianxiang3() {
+    try {
+        var input = table.querySelectorAll('tbody tr')[69].querySelectorAll('td')[7].querySelectorAll('input')[0];
+        console.log('Lianxiang2 waiting')
+        input.click();
+    } catch (e) {
+        console.log('Lianxiang2 error')
+    }
 }
